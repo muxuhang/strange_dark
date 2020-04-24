dc73e635713efcb9c93660600809838744264e68
xuhang mu <muxuhang@gmail.com>
2020-04-24 10:17:27 +0800

    new page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
+import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
@@ -8,10 +9,19 @@ import { rhythm } from "../utils/typography"
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
-  console.log(posts);
+  if (typeof window !== 'undefined') {
+    console.log(document);
+    var hm = document.createElement("script");
+    hm.src = "https://s9.cnzz.com/z_stat.php?id=1278835659&web_id=1278835659";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  }
+  
+
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
+      <Bio />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
